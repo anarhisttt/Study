21b692f2b9033052dd9d869dd79925250e11d27b
yaroslavagrebeneva <yaroslavagrebeneva@gmail.com>
2023-12-26 20:35:03 +0300

    Добавила вывод информации о КИПУ.

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main()
 {
 	cout << "My name is Slava.I am sudy in CIPU as a programmer. \n";
+	cout << "CIPU is a Crimean Ingeneering Pedagogical University named after Fevzi Yakubov. It was founded at 1993"
 }
 
